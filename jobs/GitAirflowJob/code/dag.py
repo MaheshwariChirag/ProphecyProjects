0bf143cf74c99796300bb38b74a47948da8e348f
chirag@prophecy.io <chirag@prophecy.io>
2024-09-12 10:02:44 +0000

    Renames GitAirflowJob and updates related paths and configurations

--- jobs/GitAirflowJob/code/dag.py
+++ jobs/GitAirflowJob/code/dag.py
@@ -7,11 +7,11 @@ from airflow import DAG
 from airflow.models.param import Param
 from airflow.decorators import task
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-from prashantpk3s_chirag_gitproject_gitairflowjob.tasks import Model_0
+from staging_chirag_snowprojectgit_gitairflowjob.tasks import Model_0
 PROPHECY_RELEASE_TAG = "__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__"
 
 with DAG(
-    dag_id = "prashantpk3s_chirag_GitProject_GitAirflowJob", 
+    dag_id = "staging_chirag_SnowProjectGit_GitAirflowJob", 
     schedule_interval = "0/10 * * * *", 
     default_args = {"owner" : "Prophecy", "ignore_first_depends_on_past" : True, "do_xcom_push" : True}, 
     start_date = pendulum.today('UTC'), 
--- jobs/GitAirflowJob/code/staging_chirag_snowprojectgit_gitairflowjob/tasks/Model_0.py
+++ jobs/GitAirflowJob/code/staging_chirag_snowprojectgit_gitairflowjob/tasks/Model_0.py
@@ -1,4 +1,4 @@
-from prashantpk3s_chirag_gitproject_gitairflowjob.utils import *
+from staging_chirag_snowprojectgit_gitairflowjob.utils import *
 
 def Model_0():
     from airflow.operators.python import PythonOperator
@@ -19,17 +19,17 @@ def Model_0():
           "run_children": False,
           "run_tests": True,
           "run_mode": "project",
-          "entity_kind": None,
-          "entity_name": None,
-          "project_id": "44",
-          "git_entity": "tag",
-          "git_entity_value": "__PROJECT_FULL_RELEASE_TAG_PLACEHOLDER__",
+          "entity_kind": "model",
+          "entity_name": "OrderId",
+          "project_id": "14729",
+          "git_entity": "branch",
+          "git_entity_value": "gitProject",
           "git_ssh_url": "https://github.com/MaheshwariChirag/ProphecyProjects",
           "git_sub_path": "",
           "select": "",
           "threads": "",
           "exclude": "",
-          "run_props": " --profile GitProjectFabric",
-          "envs": {"DBT_DATABRICKS_INVOCATION_ENV" : "prophecy", "DBT_PROFILES_DIR" : "GitProject"}
+          "run_props": " --profile run_profile_snowflake",
+          "envs": {"DBT_DATABRICKS_INVOCATION_ENV" : "prophecy", "DBT_PROFILES_DIR" : "/home/airflow/gcs/data"}
         },
     )
